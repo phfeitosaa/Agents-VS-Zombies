6585df05e4a6571f18df22ffe38ad216b5abb3e5
Paulo Henrique <paulo_he14@hotmail.com>
2016-09-20 18:59:27 -0300

    Tiled Level1 concluido e DPAD direito adicionado

--- level1.lua
+++ level1.lua
@@ -1,27 +1,23 @@
-display.setStatusBar( display.HiddenStatusBar )
-
 local composer = require( "composer" )
 local scene = composer.newScene()
-local json = require "json"
 local mte = require("MTE.mte").createMTE()
-
--- load the physics data:
-local physicsData = (require "physicsData").physicsData(0.5)
 
 -- forward declarations and other locals
 local screenW, screenH, halfW = display.actualContentWidth, display.actualContentHeight, display.contentCenterX
 
+display.setStatusBar( display.HiddenStatusBar )
 display.setDefault( "magTextureFilter", "nearest" )
 display.setDefault( "minTextureFilter", "nearest" )
 system.activate("multitouch")
 
 -- player and zombies declarations:
 local player1, player2, player3, zombies
+local yAxis, xAxis = 0, 0
 
 -- var current char:
 local currentChar
 
--- colision names:
+-- collision names:
 local player1Name = "player1"
 local player2Name = "player2"
 local player3Name = "player3"
@@ -29,32 +25,33 @@ local zombiesName = "zombies"
 
 -- Load the music of the game:
 soundTable = {
-	ready = audio.loadSound( "sounds/ready.ogg" )
+	music = audio.loadStream( "sounds/dark_fallout.ogg" )
 }
 
 -- Functions to setup chars:
 -- PLAYER 1: --------------------
 local loadplayer1 = function ()
-	local spriteSheet = graphics.newImageSheet("sprites/player1_gun_sprite.png", {width = 48, height = 48, numFrames = 16})
+    
+	local spriteSheet = graphics.newImageSheet("sprites/player1_gun.png", {width = 48, height = 48, numFrames = 1})
 	local sequenceData = {		
-		{name = "right", sheet = spriteSheet,  start = 1, time = 400, loopCount = 0},
-		{name = "down", sheet = spriteSheet, start = 2, time = 400, loopCount = 0},
-		{name = "left", sheet = spriteSheet, frames = {49, 50}, time = 400, loopCount = 0},
-		{name = "up", sheet = spriteSheet, frames = {61, 62}, time = 400, loopCount = 0}
+		{name = "right", sheet = spriteSheet,  frames = {1}, time = 400},
+		{name = "down", sheet = spriteSheet, frames = {1}, time = 400},
+		{name = "left", sheet = spriteSheet, frames = {1}, time = 400},
+		{name = "up", sheet = spriteSheet, frames = {1}, time = 400}
 	}
 	local player1 = display.newSprite(spriteSheet, sequenceData)
 	local setup = {
 		kind = "sprite", 
-		layer =  mte.getSpriteLayer(1), 
-		--locX = 50, 
-		--locY = 70,
+		layer = 3, 
+		locX = 15,
+		locY = 10,
 		levelWidth = 48,
-		levelHeight = 48,
-		name = "player1"
+		levelHeight = 48
 	}
-	mte.physics.addBody( player1, "dynamic", physicsData:get("hitman1_gun") )
+	mte.physics.addBody( player1, "dynamic" )
 	mte.addSprite(player1, setup)
 	mte.setCameraFocus(player1)
+	player1:setSequence("up")
 	return player1
 end
 --[[
@@ -107,6 +104,60 @@ local loadplayer3 = function ()
 end
 ]]--
 
+function touchFunction(e)
+	local eventName = e.phase
+	local direction = e.target.myName
+	local currentChar = player1
+	
+	if eventName == "began" or eventName == "moved" then
+		if direction == "up" then 
+			yAxis = -6
+			xAxis = 0
+			player1.rotation = 0
+		elseif direction == "down" then 
+			yAxis = 6
+			xAxis = 0
+			player1.rotation = 180
+		elseif direction == "right" then
+			xAxis = 6
+			yAxis = 0
+			player1.rotation = 90
+		elseif direction == "left" then
+			xAxis = -6
+			yAxis = 0
+			player1.rotation = -90
+		end
+	else 
+		yAxis = 0
+		xAxis = 0
+	end
+end
+
+function RotationFunction(e)
+	local eventName = e.phase
+	local direction = e.target.myName
+	local currentChar = player1
+	
+	if eventName == "began" or eventName == "moved" then
+		if direction == "up" then 
+			player1.rotation = 0
+		elseif direction == "down" then 
+			player1.rotation = 180
+		elseif direction == "right" then
+			player1.rotation = 90
+		elseif direction == "left" then
+			player1.rotation = -90
+		end
+	else 
+		yAxis = 0
+		xAxis = 0
+	end
+end
+
+function test(e)
+	print("eita")
+end
+
 function scene:create( event )
 	local sceneGroup = self.view
 
@@ -114,20 +165,21 @@ function scene:create( event )
 	mte.enableBox2DPhysics()
 	mte.physics.start()
 	mte.physics.setGravity(0,0)
-	mte.physics.setDrawMode("hybrid")
-	mte.physics.pause()
+	--mte.physics.setDrawMode("hybrid")
 
 	-- LOAD MAP: ----------------------------------------------------------
 	mte.toggleWorldWrapX(false)
 	mte.toggleWorldWrapY(false)
 	mte.loadMap("levels/level1.tmx") 
 	mte.drawObjects()
-	map = mte.setCamera({levelPosX = 0, levelPosY = 0, blockScale = 18})
+	map = mte.setCamera({levelPosX = 0, levelPosY = 0, blockScale = 40})
 	mte.constrainCamera()
 
 	-- LOAD CHARS: --------------------------------------------------------
 	player1 = loadplayer1()
 	player1.name = player1Name
+	player1.collision = test
+	player1:addEventListener("collision")
 	
 	--[[
 	player2 = loadplayer2()
@@ -137,107 +189,102 @@ function scene:create( event )
 	player3.name = player3Name
 	]]--
 
-	-- INSERT DPAD: -------------------------------------------------------
-	local buttons = {}
+	-- INSERT LEFT DPAD: -------------------------------------------------------
+	local leftdpad = {}
 
-	buttons[1] = display.newImage("/sprites/button.png")
-	buttons[1].x = 10
-	buttons[1].y = 245
-	buttons[1].width = 45
-	buttons[1].height = 31
-	buttons[1].myName = "up"
-	buttons[1].rotation = -90
+	leftdpad[1] = display.newImage("sprites/button.png")
+	leftdpad[1].x = 10
+	leftdpad[1].y = 245
+	leftdpad[1].width = 45
+	leftdpad[1].height = 31
+	leftdpad[1].myName = "up"
+	leftdpad[1].rotation = -90
 
-	buttons[2] = display.newImage("/sprites/button.png")
-	buttons[2].x = 10
-	buttons[2].y = 291
-	buttons[2].width = 45
-	buttons[2].height = 31
-	buttons[2].myName = "down"
-	buttons[2].rotation = 90
+	leftdpad[2] = display.newImage("sprites/button.png")
+	leftdpad[2].x = 10
+	leftdpad[2].y = 291
+	leftdpad[2].width = 45
+	leftdpad[2].height = 31
+	leftdpad[2].myName = "down"
+	leftdpad[2].rotation = 90
 
-	buttons[3] = display.newImage("/sprites/button.png")
-	buttons[3].x = -12
-	buttons[3].y = 268,5
-	buttons[3].width = 45
-	buttons[3].height = 31
-	buttons[3].myName = "left"
-	buttons[3].rotation = 180
+	leftdpad[3] = display.newImage("sprites/button.png")
+	leftdpad[3].x = -12
+	leftdpad[3].y = 268,5
+	leftdpad[3].width = 45
+	leftdpad[3].height = 31
+	leftdpad[3].myName = "left"
+	leftdpad[3].rotation = 180
 
-	buttons[4] = display.newImage("/sprites/button.png")
-	buttons[4].x = 32
-	buttons[4].y = 268,5
-	buttons[4].width = 45
-	buttons[4].height = 31
-	buttons[4].myName = "right"
+	leftdpad[4] = display.newImage("sprites/button.png")
+	leftdpad[4].x = 32
+	leftdpad[4].y = 268,5
+	leftdpad[4].width = 45
+	leftdpad[4].height = 31
+	leftdpad[4].myName = "right"
+
+
+	-- INSERT RIGHT DPAD: -------------------------------------------------------
+	local rightdpad = {}
+
+	rightdpad[1] = display.newImage("sprites/button.png")
+	rightdpad[1].x = 460
+	rightdpad[1].y = 245
+	rightdpad[1].width = 45
+	rightdpad[1].height = 31
+	rightdpad[1].myName = "up"
+	rightdpad[1].rotation = -90
+
+	rightdpad[2] = display.newImage("sprites/button.png")
+	rightdpad[2].x = 460
+	rightdpad[2].y = 291
+	rightdpad[2].width = 45
+	rightdpad[2].height = 31
+	rightdpad[2].myName = "down"
+	rightdpad[2].rotation = 90
+
+	rightdpad[3] = display.newImage("sprites/button.png")
+	rightdpad[3].x = 438
+	rightdpad[3].y = 268,5
+	rightdpad[3].width = 45
+	rightdpad[3].height = 31
+	rightdpad[3].myName = "left"
+	rightdpad[3].rotation = 180
+
+	rightdpad[4] = display.newImage("sprites/button.png")
+	rightdpad[4].x = 482
+	rightdpad[4].y = 268,5
+	rightdpad[4].width = 45
+	rightdpad[4].height = 31
+	rightdpad[4].myName = "right"
+
 
 	--all display objects must be inserted into group
 	sceneGroup:insert( map )
-	sceneGroup:insert( player1 )
+	--sceneGroup:insert( player1 )
 	--sceneGroup:insert( player2 )
 	--sceneGroup:insert( player3 )
-	sceneGroup:insert( buttons[1] )
-	sceneGroup:insert( buttons[2] )
-	sceneGroup:insert( buttons[3] )
-	sceneGroup:insert( buttons[4] )
+	sceneGroup:insert( leftdpad[1] )
+	sceneGroup:insert( leftdpad[2] )
+	sceneGroup:insert( leftdpad[3] )
+	sceneGroup:insert( leftdpad[4] )
+	sceneGroup:insert( rightdpad[1] )
+	sceneGroup:insert( rightdpad[2] )
+	sceneGroup:insert( rightdpad[3] )
+	sceneGroup:insert( rightdpad[4] )
 
-	local yAxis = 0
-	local xAxis = 0
-
-local touchFunction = function(e)
-	local eventName = e.phase
-	local direction = e.target.myName
-	local currentChar = player1
-	
-	if eventName == "began" or eventName == "moved" then
-		if direction == "up" then 
-			yAxis = -3
-			xAxis = 0
-		elseif direction == "down" then 
-			yAxis = 3
-			xAxis = 0
-		elseif direction == "right" then
-			xAxis = 3
-			yAxis = 0
-		elseif direction == "left" then
-			xAxis = -3
-			yAxis = 0
-		end
-	else 
-		yAxis = 0
-		xAxis = 0
-	end
-end
 
 local j=1
 
-for j=1, #buttons do 
-	buttons[j]:addEventListener("touch", touchFunction)
+for j=1, #leftdpad do 
+	leftdpad[j]:addEventListener("touch", touchFunction)
 end
 
-local update = function()
-    mte.update()
-    local currentChar = player1
-	currentChar.x = currentChar.x + xAxis
-	currentChar.y = currentChar.y + yAxis
-
-	if currentChar.x <= currentChar.width * .5 then 
-		currentChar.x = currentChar.width * .5
-	elseif currentChar.x >= screenW - currentChar.width * .5 then 
-		currentChar.x = screenW - currentChar.width * .5
-	end
-
-	if currentChar.y <= currentChar.height * .5 then
-		currentChar.y = currentChar.height * .5
-	elseif currentChar.y >= screenH - currentChar.height * .5 then 
-		currentChar.y = screenH - currentChar.height * .5
-	end 
+for j=1, #rightdpad do 
+	rightdpad[j]:addEventListener("touch", RotationFunction)
 end
 
-Runtime:addEventListener("enterFrame", update)
-
 end
-
 
 function scene:show( event )
 	local sceneGroup = self.view
@@ -246,11 +293,10 @@ function scene:show( event )
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		-- 
-		-- INSERT code here to make the scene come alive
+		-- Called when the scene is now on screen 
 		-- e.g. start timers, begin animation, play audio, etc.
 		mte.physics.start()
+		audio.play( soundTable["music"], {loops=-1})
 	end
 end
 
@@ -285,6 +331,9 @@ end
 
 function update()
     mte.update()
+    local currentChar = player1
+	currentChar.x = currentChar.x + xAxis
+	currentChar.y = currentChar.y + yAxis
 end
     
 
@@ -295,7 +344,7 @@ scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
---scene:addEventListener( "enterFrame", update )
+Runtime:addEventListener( "enterFrame", update )
 
 -----------------------------------------------------------------------------------------
 
